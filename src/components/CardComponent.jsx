b6de3ebe77ede78a025c442d516f1e8cf6c39475
krish <guptashanu341@gmail.com>
2025-09-04 00:39:37 +0530

    11

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CardComponent = ({ item, addToCart }) => {
   const imageUrl = item.image.startsWith("http")
@@ -6,19 +7,24 @@ const CardComponent = ({ item, addToCart }) => {
     : `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${item.image}`;
 
   return (
-    <div className="card">
-      <img
-        src={imageUrl}
-        alt={item.name}
-        className="card-img"
-      />
-      <h3>{item.name}</h3>
-      <p>{item.cuisine}</p>
-      <p>⭐ {item.rating}</p>
-      <p>⏱ {item.deliveryTime} min</p>
-      <button onClick={() => addToCart(item)}>Add to Cart</button>
-    </div>
+    <Link to={`/rest/${item.id}`} className="card-link">
+
+      <div className="card">
+        <img
+          src={imageUrl}
+          alt={item.name}
+          className="card-img"
+        />
+        <h3>{item.name}</h3>
+        <p>{item.cuisine}</p>
+        <p>⭐ {item.rating}</p>
+        <p>⏱ {item.deliveryTime} min</p>
+        <button onClick={e => {
+          e.preventDefault();
+          addToCart(item);
+        }}>Add to Cart</button>
+      </div>
+    </Link>
   );
 };
-
 export default CardComponent;
